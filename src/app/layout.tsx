a7a0cc3706d1ac9a2bc2a06e433888b8b8fb5292
Muhammad Imran Ismail <imranismail0610@gmail.com>
2025-12-05 14:27:21 +0000

    Ok it works but change the outline icon to be chef hat instead of using

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,6 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
