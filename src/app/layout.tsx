2780d4fead3c689b09eefadb84ae614b199ff9b2
Muhammad Imran Ismail <imranismail0610@gmail.com>
2025-12-05 14:19:00 +0000

    It is using the default icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'%3e%3ccircle cx='50' cy='50' r='50' fill='hsl(120 73% 75%)'/%3e%3cpath fill='hsl(224 71.4% 4.1%)' d='M68.75,75.625 C68.75,74.288 67.637,73.125 66.25,73.125 L33.75,73.125 C32.362,73.125 31.25,74.288 31.25,75.625 L31.25,81.25 L68.75,81.25 L68.75,75.625 Z M33.75,70.625 L33.75,34.375 C33.75,27.081 39.706,20.625 46.875,20.625 L50,20.625 C57.294,20.625 63.75,27.081 63.75,34.375 L63.75,70.625 M50,15 L50,23.125' stroke='hsl(224 71.4% 4.1%)' stroke-width='5' stroke-linecap='round'/%3e%3c/svg%3e" type="image/svg+xml" />
+        <link rel="icon" href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'%3E%3Ccircle cx='50' cy='50' r='50' fill='%23bbf7d0'/%3E%3Cpath fill='%230a1c57' d='M68.75 81.25V75.625c0-1.337-1.113-2.5-2.5-2.5H33.75c-1.388 0-2.5 1.163-2.5 2.5V81.25zM63.75 70.625V34.375C63.75 27.081 57.294 20.625 50 20.625h-3.125C39.706 20.625 33.75 27.081 33.75 34.375v36.25z'/%3E%3Cpath stroke='%230a1c57' stroke-width='5' stroke-linecap='round' d='M50 23.125V15'/%3E%3C/svg%3E" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
